13818093c37bc739e28c4a3d6dd334dce3c6078d
trisgd <triyashagd@gmail.com>
2019-05-29 17:10:53 +0530

    Changes made to spider to incorporate user input

--- Flipkart_spider/spiders/flipkart.py
+++ Flipkart_spider/spiders/flipkart.py
@@ -1,12 +1,16 @@
 import pickle
 from scrapy import Spider
 from scrapy.http import Request
+import sys
+from scrapy.crawler import CrawlerProcess
 
 class FlipkartSpider(Spider):
     name = 'flipkart'
     start_urls = ['https://www.flipkart.com/search?q=Laptop']
     all_data =[]
-    number = 100
+    number = (int(sys.argv[1]))-1
+    pickle_file=sys.argv[2]
+
     def parse(self, response):
         laptops = response.css("div._3O0U0u")
         flag=True
@@ -23,12 +27,14 @@ class FlipkartSpider(Spider):
                 flag=False
                 break
 
-
-
         # list of relative urls (previous and next)
         next_page= response.css('a._3fVaIS::attr(href)').getall()
         if next_page and flag:
             next_page_url = response.urljoin(next_page[-1]) # pick the "next" url
             self.log(f'relative url: {next_page_url}')
             yield Request(next_page_url, callback=self.parse)
-        pickle.dump(self.all_data,(open("pickle.data","wb")))
+        pickle.dump(self.all_data,(open("pickle_file","wb")))
+
+process= CrawlerProcess()
+process.crawl(FlipkartSpider)
+process.start()
